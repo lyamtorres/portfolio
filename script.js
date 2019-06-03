6259750d43f755a0d7bf1900c3d6e19fc9967b98
lyamtorres <lyamtorres@hotmail.comgit config --global user.email lyamtorres@hotmail.comgit config --global user.email lyamtorres@hotmail.com>
2019-06-03 15:31:50 +0200

    Responsive menu fonctionality added.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+const menu = document.querySelector('.menu');
+const burgerButton = document.querySelector('#burger-menu');
+
+function hideShow() {
+    if (menu.classList.contains('is-active')) {
+        menu.classList.remove('is-active');
+    } else {
+        menu.classList.add('is-active');
+    }
+}
+
+burgerButton.addEventListener('click', hideShow);
+
