71069cd27714bc1a092c9b4910c290f2f5270c0b
lyamtorres <lyamtorres@hotmail.comgit config --global user.email lyamtorres@hotmail.comgit config --global user.email lyamtorres@hotmail.com>
2019-07-11 10:52:00 +0200

    Header, hero and portfolio sections responsive features adjusted

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ function hideShow() {
 
 burgerButton.addEventListener('click', hideShow);
 
+var prevScrollPos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollPos > currentScrollPos) {
+        document.getElementById("navbar").style.top = 0;
+    } else {
+        document.getElementById("navbar").style.top = "-98px";
+    }
+    prevScrollPos = currentScrollPos;
+}
